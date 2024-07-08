0bb0cff2ef9e1bdaeb17011e59b3b5c8807b0712
18867681176 <549671887@qq.com>
2024-07-08 21:58:00 +0800

    commit

---
+++ libs_dist/build_arm64_so.sh
@@ -0,0 +1,4 @@
+# apt-get install gcc-10-aarch64-linux-gnu
+# build arm64 so
+GOOS=linux CGO_ENABLED=1 GOARCH=arm64 CC="aarch64-linux-gnu-gcc-10" go build -buildmode=c-shared -o requests-go-arm64.so export.go
+#set CGO_ENABLED=0 setGOOS=linux set GOARCH=amd64 go build -buildmode=c-shared -o requests-go-arm64.so export.go
